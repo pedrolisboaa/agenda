cb76a07cf052636478cd82cafddf91a6add3a3af
pedrolisboaa <phlisboa2000@gmail.com>
2023-05-24 09:39:22 -0300

    Atualizando isso aqui.

--- contato/urls.py
+++ contato/urls.py
@@ -5,6 +5,9 @@ app_name = 'contato'
 
 urlpatterns = [
     path('', index, name="index"),
-    path('<int:contato_id>/', contato, name="contato"),
     path('buscar/', buscar, name="buscar"),
+
+    # Contato (CRUD)
+    path('contato/<int:contato_id>/detail/', contato, name="contato"),
+    
 ]
